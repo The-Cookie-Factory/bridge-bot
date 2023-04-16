f19c7c672c76a7d00b5dfef0751dfc7ff56e78b9
marzeq <marzeqmarzeq@gmail.com>
2023-04-16 21:02:11 +0200

    Implement /voice claim, lock and hijack

--- src/commands/voice.ts
+++ src/commands/voice.ts
@@ -1,4 +1,4 @@
-import { partyChannelOwners } from "../events/discord/voiceStateUpdate";
+import { partyChannelLocks, partyChannelOwners } from "../events/discord/voiceStateUpdate";
 import { Command } from "../interfaces/Command";
 import { ApplicationCommandOptionType, BaseGuildVoiceChannel } from "discord.js";
 
@@ -16,6 +16,29 @@ const command: Command = {
 						name: "name",
 						description: "The new name of your party voice channel",
 						type: ApplicationCommandOptionType.String,
+						required: true,
+					},
+				],
+			},
+			{
+				name: "claim",
+				description: "Claims ownership over a party voice channel if the owner has left",
+				type: ApplicationCommandOptionType.Subcommand,
+			},
+			{
+				name: "hijack",
+				description: "Hijacks a party voice channel (staff only)",
+				type: ApplicationCommandOptionType.Subcommand,
+			},
+			{
+				name: "lock",
+				description: "Locks/unlucks a party voice channel",
+				type: ApplicationCommandOptionType.Subcommand,
+				options: [
+					{
+						name: "shouldlock",
+						description: "Whether to lock the channel or not",
+						type: ApplicationCommandOptionType.Boolean,
 						required: true,
 					},
 				],
@@ -86,23 +109,25 @@ const command: Command = {
 			return;
 		}
 
-		if (partyChannelOwners.get(voiceChannel.id) !== interaction.member.id) {
-			await interaction.reply({
-				embeds: [
-					{
-						title: "Error",
-						description: "You are not the owner of this party voice channel",
-						color: 0xff0000,
-					},
-				],
-			});
-			return;
-		}
-
 		const subcommand = interaction.options.getSubcommand(true);
+
+		const isOwner = partyChannelOwners.get(voiceChannel.id) !== interaction.member.id;
 
 		switch (subcommand) {
 			case "name": {
+				if (isOwner) {
+					await interaction.reply({
+						embeds: [
+							{
+								title: "Error",
+								description: "You are not the owner of this party voice channel",
+								color: 0xff0000,
+							},
+						],
+					});
+					return;
+				}
+
 				const name = interaction.options.getString("name", true);
 
 				if (name.length > 24) {
@@ -125,6 +150,133 @@ const command: Command = {
 						{
 							title: "Changed the name",
 							description: `Changed the name of your party voice channel to \`${name}\``,
+							color: 0x00ff00,
+						},
+					],
+				});
+
+				break;
+			}
+			case "claim": {
+				const owner = partyChannelOwners.get(voiceChannel.id);
+
+				if (owner === interaction.member.id) {
+					await interaction.reply({
+						embeds: [
+							{
+								title: "Error",
+								description: "You are already the owner of this party voice channel",
+								color: 0xff0000,
+							},
+						],
+					});
+					return;
+				}
+
+				if (owner && voiceChannel.members.has(owner)) {
+					await interaction.reply({
+						embeds: [
+							{
+								title: "Error",
+								description: "The owner of this party voice channel is still in the channel",
+								color: 0xff0000,
+							},
+						],
+					});
+					return;
+				}
+
+				partyChannelOwners.set(voiceChannel.id, interaction.member.id);
+
+				await interaction.reply({
+					embeds: [
+						{
+							title: "Claimed the channel",
+							description: "You are now the owner of this party voice channel",
+							color: 0x00ff00,
+						},
+					],
+				});
+
+				break;
+			}
+			case "hijack": {
+				const allowedRoles = [
+					"778750457291997204", // admin role
+					"569670445012353035", // community manager role
+				];
+
+				const roles = interaction.member.roles.cache.map((role) => role.id);
+
+				if (!roles.some((role) => allowedRoles.includes(role))) {
+					await interaction.reply({
+						embeds: [
+							{
+								title: "Not for you :)",
+								description: "This is staff only",
+								color: 0xff0000,
+							},
+						],
+					});
+					return;
+				}
+
+				partyChannelOwners.set(voiceChannel.id, interaction.member.id);
+
+				await interaction.reply({
+					embeds: [
+						{
+							title: "Took over the channel",
+							description: "You are now the owner of this party voice channel",
+							color: 0x00ff00,
+						},
+					],
+				});
+
+				break;
+			}
+			case "lock": {
+				if (isOwner) {
+					await interaction.reply({
+						embeds: [
+							{
+								title: "Error",
+								description: "You are not the owner of this party voice channel",
+								color: 0xff0000,
+							},
+						],
+					});
+					return;
+				}
+
+				const locked = partyChannelLocks.get(voiceChannel.id)!;
+
+				const shouldLock = interaction.options.getBoolean("shouldlock", true);
+
+				if (locked === shouldLock) {
+					await interaction.reply({
+						embeds: [
+							{
+								title: "Error",
+								description: `This party voice channel is already ${locked ? "locked" : "unlocked"}`,
+								color: 0xff0000,
+							},
+						],
+					});
+					return;
+				}
+
+				partyChannelLocks.set(voiceChannel.id, shouldLock);
+
+				voiceChannel.permissionOverwrites.edit(interaction.guild.roles.everyone, {
+					Connect: !shouldLock,
+				});
+
+				await interaction.reply({
+					embeds: [
+						{
+							title: `${shouldLock ? "Locked" : "Unlocked"} the channel`,
+							description: `You ${shouldLock ? "locked" : "unlocked"} this party voice channel`,
 							color: 0x00ff00,
 						},
 					],
