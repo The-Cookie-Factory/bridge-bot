881be470702b1d33564a8eae90a98b3e45adeadd
marzeq <marzeqmarzeq@gmail.com>
2023-04-16 15:18:15 +0200

    Prune empty channels after reboot

--- src/events/discord/ready.ts
+++ src/events/discord/ready.ts
@@ -1,5 +1,5 @@
 import { Event } from "../../interfaces/Event";
-import { TextChannel } from "discord.js";
+import { BaseGuildVoiceChannel, ChannelType, TextChannel } from "discord.js";
 
 export default {
 	name: "ready",
@@ -11,5 +11,21 @@ export default {
 		bot.officerChannel = (await bot.discord.channels.fetch(process.env.OFFICER_CHANNEL_ID)) as TextChannel;
 
 		bot.setStatus();
+
+		const partyCreateChannel = (await bot.discord.channels
+			.fetch(process.env.PARTY_CHANNEL_ID)
+			.catch(null)) as BaseGuildVoiceChannel | null;
+
+		if (partyCreateChannel && partyCreateChannel.parent) {
+			// fetch all
+			const empty = Array.from(partyCreateChannel.parent.children.cache.values())
+				.filter((c) => c.id !== partyCreateChannel.id)
+				.filter((c) => c.type === ChannelType.GuildVoice)
+				.filter((c) => c.members.size === 0);
+
+			for (const channel of empty) {
+				await channel.delete().catch(null);
+			}
+		}
 	},
 } as Event;
